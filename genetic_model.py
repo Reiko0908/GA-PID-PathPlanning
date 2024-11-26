4290ae3c6fca8ae9a22e626274a2f2fa67177426
Pacman Pi <haonhien3106@gmail.com>
2024-11-26 12:53:26 +0700

    [edit files] new chromosome representation

--- genetic_model.py
+++ genetic_model.py
@@ -12,46 +12,44 @@ PATH_SMOOTHNESS_PRIORITIZE_FACTOR = 0.1
 
 CHROMOSOME_CROSSOVER_SELECTION_RATIO = 0.8
 ELITISM_RATIO = 0.04
-POPULATION = 50
+POPULATION = 200
+CHROMOSOME_INITIAL_LENGTH = CHROMOSOME_MAX_LENGTH // 2
 
 def chromosome_to_bezier(chromosome):
-    bezier = Bezier()
-    sampled_indices = [i for i, x in enumerate(chromosome) if x == 1]
+    return
 
-    if len(sampled_indices) != BEZIER_ORDER + 1:
-        raise ValueError(f"Chromosome must encode exactly {BEZIER_ORDER + 1} control points.")
-    bezier.control_points = np.array([
-        [index % SCREEN_WIDTH, index // SCREEN_WIDTH]
-        for index in sampled_indices
-    ])
-
-    return bezier
+class Chromosome:
+    def __init__(self):
+        self.genes = []
+        return
 
 class Genetic_model:
     def __init__(self):
         print("Initializing Model")
         self.chromosomes = []
-        self.start_gene = START_POSITION[0] * SCREEN_HEIGHT + START_POSITION[1] # index in chormosome that indicates car start position
-        self.end_gene = END_POSITION[0] * SCREEN_HEIGHT + END_POSITION[1] # index in chormosome that indicates car end position
-        self.chromosome_length = SCREEN_HEIGHT * SCREEN_WIDTH # length of each chromosome
 
     def generate_initial_population(self):
         print("Generating Intial Chromosomes")
         for _ in range(POPULATION):
-            chromosome = np.zeros(SCREEN_HEIGHT * SCREEN_WIDTH).tolist()
-            chromosome[self.start_gene] = 1
-            chromosome[self.end_gene] = 1
+            chromosome = Chromosome().genes.append(START_POSITION) 
+            while(True):
 
-            num_control_points = BEZIER_ORDER + 1
-            while(True): # Loop forever until the random genes dont match start and end genes
-                control_genes = np.random.randint(SCREEN_HEIGHT * SCREEN_WIDTH, size = num_control_points - 2).tolist()
-                if self.start_gene not in control_genes and self.end_gene not in control_genes:
-                    break
-            
-            for gene in control_genes:
-                chromosome[gene] = 1
 
-            self.chromosomes.append(chromosome)
+        # for _ in range(POPULATION):
+        #     chromosome = np.zeros(SCREEN_HEIGHT * SCREEN_WIDTH).tolist()
+        #     chromosome[CHROMOSOME_START_GENE] = 1
+        #     chromosome[CHROMOSOME_END_GENE] = 1
+        #
+        #     num_control_points = BEZIER_ORDER + 1
+        #     while(True): # Loop forever until the random genes dont match start and end genes
+        #         control_genes = np.random.randint(SCREEN_HEIGHT * SCREEN_WIDTH, size = num_control_points - 2).tolist()
+        #         if CHROMOSOME_START_GENE not in control_genes and CHROMOSOME_END_GENE not in control_genes:
+        #             break
+        #
+        #     for gene in control_genes:
+        #         chromosome[gene] = 1
+        #
+        #     self.chromosomes.append(chromosome)
 
     def fitness_function(self, chromosome, map):
         bezier = chromosome_to_bezier(chromosome)
@@ -77,64 +75,10 @@ class Genetic_model:
         return elite_indices, elites
 
     def crossover(self, map):
-        print("Performing Crossover")
-        fitness_scores = self.evaluate_population(map)
-        elite_indices, elites = self.select_elites(fitness_scores)
-        non_elite_indices = np.setdiff1d(np.arange(len(self.chromosomes)), elite_indices)        
-
-        num_crossover = int(len(non_elite_indices)*CHROMOSOME_CROSSOVER_SELECTION_RATIO)
-        selected_indices = np.random.choice(
-                len(non_elite_indices), num_crossover, replace=False
-        )
-        selected_chromosomes = [self.chromosomes[i] for i in selected_indices]
-        np.random.shuffle(selected_chromosomes)
-        
-        new_chromosomes = []
-        for i in range (0, len(selected_chromosomes) -1, 2):
-            parent1 = selected_chromosomes[i]
-            parent2 = selected_chromosomes[i+1]
-
-            points = np.random.choice(
-                range(1, self.chromosome_length - 1),2, replace=False
-            )
-            point1, point2 = sorted(points)
-
-            child1 = parent1[:point1] + parent2[point1:point2] + parent1[point2:]
-            child2 = parent2[:point1] + parent1[point1:point2] + parent2[point2:]
-            
-            child1[self.start_gene] = 1
-            child1[self.end_gene] = 1
-            child2[self.start_gene] = 1
-            child2[self.end_gene] = 1
-
-            new_chromosomes.extend([child1, child2])
-
-            num_replacement = len(self.chromosomes) - len(elites)
-            self.chromosomes = elites + new_chromosomes[:num_replacement]
+        return
 
     def mutation(self):
-        generation_mutate_ratio = np.random.random()
-        chromosome_mutate_ratio = np.random.uniform(0.1, 0.5)
-        gene_mutate_ratio = np.random.uniform(0.02,0.3)
-
-        if generation_mutate_ratio < chromosome_mutate_ratio:
-            print("Mutation skipped for this generation.")
-            return
-        print("Performing mutation for this generation.")
-        num_chromosome_mutate = int(len(self.chromosomes) * chromosome_mutate_ratio)
-        selected_chromosomes_indices = np.random.choice(
-                len(self.chromosomes), num_chromosome_mutate, replace=False
-        )
-        for idx in selected_chromosomes_indices: 
-            chromosome = self.chromosomes[idx]
-            num_gene_mutate = int(self.chromosome_length * gene_mutate_ratio)
-            selected_genes_indices = np.random.choice(
-                    range(1, self.chromosome_length - 1), num_gene_mutate, replace=False
-        )
-            for gene_idx in selected_genes_indices:
-                chromosome[gene_idx] = 1 - chromosome[gene_idx]
-
-                self.chromosomes[idx] = chromosome
+        return
 
     def validate(self):
         return
--- train.py
+++ train.py
@@ -20,20 +20,7 @@ def plot_terrain(map):
     plt.scatter(START_POSITION[0], START_POSITION[1], color=map_end_points_color)
     plt.scatter(END_POSITION[0], END_POSITION[1], color=map_end_points_color)
 
-    plt.show()
-
-def plot_bezier_with_terrain(map, bezier):
-    # Plot the terrain
-    plt.figure(figsize=(SCREEN_WIDTH/100, SCREEN_HEIGHT/100))
-    obstacle_color = (OBSTACLE_COLOR[0]/255, OBSTACLE_COLOR[1]/255, OBSTACLE_COLOR[2]/255)
-    for obs in map.obstacles:
-        plt.scatter(obs.position[0], obs.position[1], s=obs.radius**2, color=obstacle_color)
-
-    map_end_points_color = (MAP_END_POINTS_COLOR[0]/255, MAP_END_POINTS_COLOR[1]/255, MAP_END_POINTS_COLOR[2]/255)
-    plt.scatter(START_POSITION[0], START_POSITION[1], color=map_end_points_color)
-    plt.scatter(END_POSITION[0], END_POSITION[1], color=map_end_points_color)
-
-    # Plot the bezier
+def plot_bezier(bezier):
     t_values = np.linspace(0, 1, BEZIER_RESOLUTION)
     local_points = np.zeros((BEZIER_RESOLUTION, 2))
     bezier_local_point_color = (BEZIER_LOCAL_POINT_COLOR[0]/255, BEZIER_LOCAL_POINT_COLOR[1]/255, BEZIER_LOCAL_POINT_COLOR[2]/255)
@@ -45,7 +32,6 @@ def plot_bezier_with_terrain(map, bezier):
             local_points[i] = local_points[i] + bezier.control_points[j] * temp
     
     plt.plot(local_points[:, 0], local_points[:, 1], color=bezier_local_point_color)
-    plt.show()
 
 if __name__ == "__main__":
 
@@ -53,10 +39,19 @@ if __name__ == "__main__":
     map.create_obstacles()
     # map.save_terrain("terrain.txt")
     map.load_terrain("terrain.txt")
-    map.generate_danger_map()
+    # map.generate_danger_map()
 
     model = Genetic_model()
     model.generate_initial_population()
-    print(model.evaluate_population(map))
+
+    test_bezier = Bezier()
+    test_bezier.randomize()
+    
+    # model.evaluate_population()
+
+    plot_terrain(map)
+    plot_bezier(test_bezier)
+
+    plt.show()
 
 
